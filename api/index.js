52f5b3a59b1c1da0fbc2452154e72334726c0110
addi-anant <126857686+addi-anant@users.noreply.github.com>
2023-05-14 10:15:31 +0000

    testing port

--- api/index.js
+++ api/index.js
@@ -1,46 +1,46 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const multer = require("multer");
-const path = require("path");
+// const dotenv = require("dotenv");
+// const mongoose = require("mongoose");
+// const multer = require("multer");
+// const path = require("path");
+var port = 3000;
+//Route Imports:
+// const authRoute = require("./routes/auth");
+// const userRoute = require("./routes/users");
+// const postRoute = require("./routes/posts");
+// const categoryRoute = require("./routes/categories");
 
-// Route Imports:
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users");
-const postRoute = require("./routes/posts");
-const categoryRoute = require("./routes/categories");
+// dotenv.config();
+// app.use(express.json());
+// // app.use("/images", express.static(path.join(__dirname, "/images")));
 
-dotenv.config();
-app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "/images")));
+// // mongoose
+// //   .connect(process.env.MONGO_URL)
+// //   .then(console.log("Connected to MongoDB"))
+// //   .catch((err) => console.log(err));
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "images");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, req.body.name);
+//   },
+// });
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
+// const upload = multer({ storage: storage });
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//   res.status(200).json("File has been uploaded");
+// });
+app.use("/", (req, res) => {
+  res.send("Welcome to the homepage.11");
 });
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/posts", postRoute);
+// app.use("/api/categories", categoryRoute);
 
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
-});
-app.get("/", (req, res) => {
-  res.send("Welcome to the homepage.");
-});
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/categories", categoryRoute);
-
-app.listen("5000", () => {
-  console.log("Backend is running on port 5000.");
+app.listen(port, () => {
+  console.log("Backend is running on port 8080.");
 });
